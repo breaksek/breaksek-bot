f8274965d513eddc5e9ad1b59234f2e03af44c66
breaksek <98497506+breaksek@users.noreply.github.com>
2024-08-02 17:47:24 +0700

    Update _anticall.js

--- plugins/_anticall.js
+++ plugins/_anticall.js
@@ -19,7 +19,7 @@ handler.all = async function (m) {
   if (!setting.anticall) return 
 
   if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-    await this.reply(m.chat, '*Your number is blocked!*\nSee you next time👋', null)
+    await this.reply(m.chat, '*FITUR ANTICALL*\n\nNomor anda terblokir!\nsampai jumpa👋', null)
     await delay(1000)
     await this.updateBlockStatus(m.chat, "block")
   }
